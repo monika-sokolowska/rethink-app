098a2bdf96be62985ae7b0196e10bc6d396d12e0
monika-sokolowska <monika.sokolowska@student.pk.edu.pl>
2025-11-29 12:39:56 +0100

    Revert unnecessary changes

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,6 +1,5 @@
 package com.example.backend;
 
-import com.example.backend.config.EnvFileLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -9,8 +8,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		EnvFileLoader.loadEnvFiles();
-		
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
